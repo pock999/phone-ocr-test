d43544c3e8e8b950341cc820d938ff67e36df681
rick <rickhou44@gmai.com>
2019-12-13 09:52:04 +0800

    deploy for heroku

---
+++ PhoneOcrEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rick444\desktop\rickhou\for_lab_phone_ocr\phoneocrenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
